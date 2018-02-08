4b349a00c9a3369a1027ff9715b79299cd2a6227
Serena <serena.perez0@gmail.com>
2018-02-07 21:09:34 -0800

    Update Ass3Cone.js

--- Assignment-3/Ass3Cone.js
+++ Assignment-3/Ass3Cone.js
@@ -12,17 +12,6 @@ function init() {
         return;
     }
     
-    this.program =initShaders(gl,"Cone-vertex-shader","Cone-fragment-shader");
-
-    this.positions.buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER,this.positions.buffer);
-    gl.bufferData(gl.ARRAY_BUFFER,newFloat32Array(positions), gl.STATIC_DRAW);
- 
-    this.indices.buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER,this.indices.buffer);
-    gl.bufferData(gl.ARRAY_BUFFER,newUint16Array(indices), gl.STATIC_DRAW
-    this.positions.attribute = gl.getAttribLocation(this.program,"vPosition");
-    
     gl.clearColor( 1.0, 1.0, 0.0, 1.0 );
 
     render();
